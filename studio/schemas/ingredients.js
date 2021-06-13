9a6f214589083cc633a781e7afa7d4defb69b146
Vishesh <vishesht27@gmail.com>
2021-06-13 19:05:14 +0530

    first commit

---
+++ studio/schemas/ingredients.js
@@ -0,0 +1,35 @@
+export default {
+  name: "ingredients",
+
+  title: "Ingredient",
+
+  type: "document",
+
+  fields: [
+    {
+      name: "name",
+
+      title: "Ingredient Name",
+
+      type: "string",
+    },
+    {
+      name: "image",
+
+      title: "Image",
+
+      type: "image",
+
+      option: {
+        hotspot: true,
+      },
+    },
+    {
+        name:"notes",
+
+        title:"Notes",
+
+        type:"text",
+    }
+  ],
+};
